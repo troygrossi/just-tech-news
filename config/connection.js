efc435dcf4248080312bc8c7f0235d98cb534c6f
Troy Grossi <ttroygrossit@gmail.com>
2021-07-20 18:52:27 -0700

    added routes, api is functional

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,8 @@
-// import the Sequelize constructor from the library
 const Sequelize = require("sequelize");
 
-// create connection to our database, pass in your MySQL information for username and password
+require("dotenv").config();
+
+// create connection to our db
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
